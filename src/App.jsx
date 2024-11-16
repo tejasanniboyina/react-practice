156fa2a9ded48ee16dd00df86724912f3e71feab
axlotlx <tejayadav435@gmail.com>
2024-11-16 19:21:43 +0530

    ...

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+
+
+
+function App() {
+ return(
+  <>
+    <Header></Header>
+    <Food></Food>
+    <Footer></Footer>
+  </>
+ );
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+return(
+    <footer>
+        <p>&copy; {new Date().getFullYear()} Your website</p>
+    </footer>
+)
+}
+
+export default Footer
